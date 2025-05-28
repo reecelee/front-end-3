db50c5454a29ba5c0c306d6940d3a96021e14d7f
Reece Lee <reecelee0128@gmail.com>
2025-05-27 21:51:52 -1000

    Add form for user input

--- users/src/App.js
+++ users/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  return <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item"></input>
     </div>
-  );
+  </form>
 }
-
-export default App;
