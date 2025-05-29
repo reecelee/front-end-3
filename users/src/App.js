897fc696755faf9f4b3d2cb024577dc3f1802f24
Reece Lee <reecelee0128@gmail.com>
2025-05-28 22:35:44 -1000

    Create form component

--- users/src/App.js
+++ users/src/App.js
@@ -1,17 +1,14 @@
 import { useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
-  function handleSubmit(e) {
-    e.preventDefault()
 
+  function addTodo(title) {
     setTodos(currentTodos => {
-    return [...currentTodos, {id:crypto.randomUUID(), title: newItem, completed: false},]
+    return [...currentTodos, {id:crypto.randomUUID(), title, completed: false},]
     }) 
-    setNewItem("")
   }
-
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
@@ -30,15 +27,10 @@ export default function App() {
   }
   
   return <>
-  <form onSubmit={handleSubmit} className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input value={newItem} onChange={e => setNewItem(e.target.value)} id="item"></input>
-    </div>
-    <button className="btn">Add</button>
-  </form>
+  <NewTodoForm onSubmit={addTodo}></NewTodoForm>
   <h1 className="header">Todo List</h1>
   <ul className="list">
+    {todos.length === 0 && "No Todos"}
     {todos.map(todo => {
       return <li key={todo.id}>
     <label>
