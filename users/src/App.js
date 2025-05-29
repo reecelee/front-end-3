efcf8dd322c5fd1017acccd3860f75ee781796d8
Reece Lee <reecelee0128@gmail.com>
2025-05-28 22:08:21 -1000

    Add logic when we submit the form

--- users/src/App.js
+++ users/src/App.js
@@ -2,9 +2,19 @@ import { useState } from "react"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+  function handleSubmit(e) {
+    e.preventDefault()
 
+    setTodos(currentTodos => {
+    return [...currentTodos, {id:crypto.randomUUID(), title: newItem, completed: false},]
+    }) 
+
+    console.log(todos)
+  }
+  
   return <>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
       <input value={newItem} onChange={e => setNewItem(e.target.value)} id="item"></input>
