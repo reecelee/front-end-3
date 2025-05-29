b1f407212cb391c6b3b6a079e66cc1b854d096a6
Reece Lee <reecelee0128@gmail.com>
2025-05-28 21:54:11 -1000

    Add checkbox item

--- users/src/App.js
+++ users/src/App.js
@@ -1,8 +1,20 @@
 export default function App() {
-  return <form className="new-item-form">
+  return <>
+  <form className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
       <input type="text" id="item"></input>
     </div>
+    <button className="btn">Add</button>
   </form>
+  <h1 className="header">Todo List</h1>
+  <ul>
+    <li>
+    <label>
+    <input type="checkbox"></input>
+    Item 1
+    </label>
+    </li>
+  </ul>
+  </>
 }
